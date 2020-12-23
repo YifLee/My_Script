1365ae9a0f9adbe5fcb4671f9513e0cfa0878ae6
YifLee <54547317+YifLee@users.noreply.github.com>
2020-12-23 19:42:04 +0800

    Update Mi_Sports.py

--- Mi_Sports.py
+++ Mi_Sports.py
@@ -144,7 +144,7 @@ def push_wx(sckey, desp=""):
 if __name__ ==  "__main__":
     # ServerChan
     sckey = input()
-    if str(sckey) == '0':
+    if str(sckey) == '1':
         sckey = ''
     # 用户名（格式为 13800138000）
     user = input()
